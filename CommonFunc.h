6ab8662074c5ecb5587e48411cbc23f68bacd392
mingduongdz <24020166@vnu.edu.vn>
2025-03-19 18:42:38 +0700

    Add files via upload

---
+++ CommonFunc.h
@@ -0,0 +1,13 @@
+#ifndef COMMONFUNC_H
+#define COMMONFUNC_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
+#include <sstream>
+#include "Texture.h"
+#include "Appconfig.h"
+
+std::string convertIntToString(int num);
+
+#endif // COMMONFUNC_H
