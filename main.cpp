1d37d90564f914d027d5e2fb20486e4482115e92
mingduongdz <24020166@vnu.edu.vn>
2025-03-19 18:31:32 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "App.h"
+//Đây là điểm bắt đầu của chương trình.
+
+//main() gọi app.run(), bắt đầu vòng lặp chính của game.
+App app;
+
+int main(int argc, char** args){
+	app.run();
+	return 0;
+}
